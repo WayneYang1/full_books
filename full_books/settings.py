04cc27a087e72d68b50a2a7f89af04e8794cad50
Ubuntu <ubuntu@ip-172-31-28-187.us-east-2.compute.internal>
2017-06-28 10:52:04 +0000

    made migrations

--- full_books/settings.py
+++ full_books/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$p7d7wdmpugy=rs40)p&540vn)0837^wb7@3)^s+jxe+!ur5p4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['18.220.26.82']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
